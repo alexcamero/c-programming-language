507df8e1b1f7447239346262017626c8c189e11f
Alex Cameron <alexcamero@gmail.com>
2022-08-14 16:27:39 -0500

    Adds a bunch to calculator

--- chapter-4/03-reverse-polish-calculator.c
+++ chapter-4/03-reverse-polish-calculator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 /* Reverse Polish Calculator */
 
@@ -12,7 +13,13 @@ double atof(char s[]);
 void push(double x);
 double pop(void);
 double calculator(void);
-double argument_error_handler(int index, int num_args);
+double number_arguments_error(int index, int num_args);
+double unknown_operation_error(int index);
+double improper_argument_error(int index, int arg_position, double arg);
+void clear_stack(void);
+void print_top_stack(void);
+double duplicate_top_stack(void);
+void swap_top_two_stack(void);
 
 int main() {
     char c;
@@ -41,8 +48,8 @@ int main() {
 }
 
 double calculator(void) {
-    int i = 0, j = 0;
-    double x;
+    int i = 0, j = 0, k = 0, signature = 0;
+    double x, y;
     char buffer[MAXLINE];
     for (j = 0; j < MAXLINE; j++) {
         buffer[j] = '\0';
@@ -58,41 +65,234 @@ double calculator(void) {
                             break;
                         case '+':
                             if (stack_length < 2) {
-                                return argument_error_handler(i-1, 2);
+                                return number_arguments_error(i-1, 2);
                             }
                             push(pop() + pop());
                             break;
                         case '-':
                             if (stack_length < 2) {
-                                return argument_error_handler(i-1, 2);
+                                return number_arguments_error(i-1, 2);
                             }
                             x = pop();
                             push(pop() - x);
                             break;
                         case '*':
                             if (stack_length < 2) {
-                                return argument_error_handler(i-1, 2);
+                                return number_arguments_error(i-1, 2);
                             }
                             push(pop() * pop());
                             break;
                         case '/':
                             if (stack_length < 2) {
-                                return argument_error_handler(i-1, 2);
+                                return number_arguments_error(i-1, 2);
                             }
                             x = pop();
                             if (x == 0.0) {
-                                stack_length = 0;
-                                printf("**********\nDivision by zero error!\n**********\n");
-                                return 0.0;
+                                return improper_argument_error(i-1, 2, x);
                             }
                             push(pop() / x);
+                            break;
+                        case '%':
+                            if (stack_length < 2) {
+                                return number_arguments_error(i-1, 2);
+                            }
+                            x = pop();
+                            push((int) pop() % (int) x);
                             break;
                         default:
                             push(atof(buffer));
                             break;
                     }
-                } else {
+                } else if ((buffer[0] >= '0' && buffer[0] <= '9') || buffer[0] == '-' || buffer[0] == '+') {
                     push(atof(buffer));
+                } else {
+                    for (k = 0; k < j; k++) {
+                        signature *= 37;
+                        if (buffer[k] <= 'z' && buffer[k] >= 'a') {
+                            signature += (buffer[k] - 'a' + 1);
+                        } else if (buffer[k] <= '9' && buffer[k] >= '0') {
+                            signature += (buffer[k] - '0' + 27);
+                        }
+                    }
+                    switch (signature) {
+                        case 37*37*('s'-'a'+1)+37*('i'-'a'+1)+('n'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(sin(pop()));
+                            break;
+                        case 37*37*('c'-'a'+1)+37*('o'-'a'+1)+('s'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(cos(pop()));
+                            break;
+                        case 37*37*('t'-'a'+1)+37*('a'-'a'+1)+('n'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            if (cos(x) == 0) {
+                                return improper_argument_error(i-j, 1, x);
+                            }
+                            push(tan(x));
+                            break;
+                        case 37*37*37*('a'-'a'+1)+37*37*('s'-'a'+1)+37*('i'-'a'+1)+('n'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            if (x > 1 || x < -1) {
+                                return improper_argument_error(i-j, 1, x);
+                            }
+                            push(asin(x));
+                            break;
+                        case 37*37*37*('a'-'a'+1)+37*37*('c'-'a'+1)+37*('o'-'a'+1)+('s'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            if (x > 1 || x < -1) {
+                                return improper_argument_error(i-j, 1, x);
+                            } 
+                            push(acos(x));
+                            break;
+                        case 37*37*37*('a'-'a'+1)+37*37*('t'-'a'+1)+37*('a'-'a'+1)+('n'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(atan(pop()));
+                            break;
+                        case 37*37*37*37*('a'-'a'+1)+37*37*37*('t'-'a'+1)+37*37*('a'-'a'+1)+37*('n'-'a'+1)+29:
+                            if (stack_length < 2) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            if (x == 0) {
+                                return improper_argument_error(i-j, 2, x);
+                            }
+                            push(atan2(pop(), x));
+                            break;
+                        case 37*37*37*('s'-'a'+1)+37*37*('i'-'a'+1)+37*('n'-'a'+1)+('h'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(sinh(pop()));
+                            break;
+                        case 37*37*37*('c'-'a'+1)+37*37*('o'-'a'+1)+37*('s'-'a'+1)+('h'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(cosh(pop()));
+                            break;
+                        case 37*37*37*('t'-'a'+1)+37*37*('a'-'a'+1)+37*('n'-'a'+1)+('h'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(tanh(pop()));
+                            break;
+                        case 37*37*('e'-'a'+1)+37*('x'-'a'+1)+('p'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(exp(pop()));
+                            break;
+                        case 37*37*('l'-'a'+1)+37*('o'-'a'+1)+('g'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            if (x <= 0) {
+                                return improper_argument_error(i-j, 1, x);
+                            }
+                            push(log(x));
+                            break;
+                        case 37*37*37*37*('l'-'a'+1)+37*37*37*('o'-'a'+1)+37*37*('g'-'a'+1)+37*28+27:
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            if (x <= 0) {
+                                return improper_argument_error(i-j, 1, x);
+                            }
+                            push(log10(x));
+                            break;
+                        case 37*37*('p'-'a'+1)+37*('o'-'a'+1)+37*37*('w'-'a'+1):
+                            if (stack_length < 2) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            y = pop();
+                            if (y == 0 && x <= 0) {
+                                return improper_argument_error(i-j, 2, x);
+                            }
+                            if (y < 0 && x != floor(x)) {
+                                return improper_argument_error(i-j, 1, y);
+                            }
+                            push(pow(y, x));
+                            break;
+                        case 37*37*37*('s'-'a'+1)+37*37*('q'-'a'+1)+37*('r'-'a'+1)+('t'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            if (x < 0) {
+                                return improper_argument_error(i-j, 1, x);
+                            }
+                            push(sqrt(x));
+                            break;
+                        case 37*37*37*('c'-'a'+1)+37*37*('e'-'a'+1)+37*('i'-'a'+1)+('l'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(ceil(pop()));
+                            break;
+                        case 37*37*37*37*('f'-'a'+1)+37*37*37*('l'-'a'+1)+37*37*('o'-'a'+1)+37*('o'-'a'+1)+('r'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(floor(pop()));
+                            break;
+                        case 37*37*37*('f'-'a'+1)+37*37*('a'-'a'+1)+37*('b'-'a'+1)+('s'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(fabs(pop()));
+                            break;
+                        case 37*37*37*37*('l'-'a'+1)+37*37*37*('d'-'a'+1)+37*37*('e'-'a'+1)+37*('x'-'a'+1)+('p'-'a'+1):
+                            if (stack_length < 2) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            push(ldexp(pop(), x));
+                            break;
+                        case 37*37*37*37*('f'-'a'+1)+37*37*37*('r'-'a'+1)+37*37*('e'-'a'+1)+37*('x'-'a'+1)+('p'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(frexp(pop(), &signature));
+                            break;
+                        case 37*37*37*('m'-'a'+1)+37*37*('o'-'a'+1)+37*('d'-'a'+1)+('f'-'a'+1):
+                            if (stack_length < 1) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            push(modf(pop(), &x));
+                            break;
+                        case 37*37*37*('f'-'a'+1)+37*37*('m'-'a'+1)+37*('o'-'a'+1)+('d'-'a'+1):
+                            if (stack_length < 2) {
+                                return number_arguments_error(i-j, 1);
+                            }
+                            x = pop();
+                            if (x == 0) {
+                                return improper_argument_error(i-j, 2, x);
+                            }
+                            push(fmod(pop(), x));
+                            break;
+                        default:
+                            return unknown_operation_error(i-j);
+                            break;
+                    }
+                    signature = 0;
                 }
                 for (j = 0; j < MAXLINE; j++) {
                     buffer[j] = '\0';
@@ -177,8 +377,42 @@ void push(double x) {
     stack[stack_length++] = x;
 }
 
-double argument_error_handler(int index, int num_args) {
+double number_arguments_error(int index, int num_args) {
     stack_length = 0;
     printf("**********\nOperation at position %d requires %d arguments.\n**********\n", index, num_args);
     return 0.0;
+}
+
+double unknown_operation_error(int index) {
+    stack_length = 0;
+    printf("**********\nUnknown operation at position %d.\n**********\n", index);
+    return 0.0;
+}
+
+double improper_argument_error(int index, int arg_position, double arg) {
+    stack_length = 0;
+    printf("**********\nImproper argument for operation at position %d. Cannot take %f for parameter %d.\n**********\n", index, arg, arg_position);
+    return 0.0;
+}
+
+void clear_stack(void) {
+    int i = 0;
+    while (stack[i] != 0.0) {
+        stack[i++] = 0.0;
+    }
+    stack_length = 0;
+}
+
+void print_top_stack(void) {
+    printf("%f", stack[stack_length - 1]);
+}
+
+double duplicate_top_stack(void) {
+    return stack[stack_length - 1];
+}
+
+void swap_top_two_stack(void) {
+    double temp = stack[stack_length - 1];
+    stack[stack_length - 1] = stack[stack_length - 2];
+    stack[stack_length - 2] = temp;
 }
