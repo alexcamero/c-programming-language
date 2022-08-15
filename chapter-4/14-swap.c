e5826d612a50bbc1d0e862019cbaa8cde75f0e97
Alex Cameron <alexcamero@gmail.com>
2022-08-15 14:22:17 -0500

    Use the block hint to make var reusable

--- chapter-4/14-swap.c
+++ chapter-4/14-swap.c
@@ -2,14 +2,18 @@
 
 /* 4-14 Define a macro swap(t,x,y) that interchanges two arguments x and y of type t */
 
-#define swap(t,x,y) t temp = y;\
+#define swap(t,x,y) {t temp = y;\
                     y = x;\
-                    x = temp;
+                    x = temp;}
 
 int main() {
     int i = 9, j = -8;
     printf("i = %d, j = %d\n", i, j);
     swap(int, i, j)
     printf("i = %d, j = %d\n", i, j);
+    double f = 9.078, g = -8.234;
+    printf("f = %f, g = %f\n", f, g);
+    swap(double, f, g)
+    printf("f = %f, g = %f\n", f, g);
     return 0;
 }
