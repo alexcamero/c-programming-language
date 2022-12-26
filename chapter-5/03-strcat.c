004ca5088bc88577f4d4868494be170e7d948e3d
Alex Cameron <alexcamero@gmail.com>
2022-12-26 13:42:23 -0600

    add chap 5, prob 3

---
+++ chapter-5/03-strcat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* 
+5-3 Write pointer version of strcat(s, t) that copies string t to the end of string s
+*/
+
+#define MAXSTRING 1000
+
+void my_strcat(char *s, char *t);
+
+int main() {
+    char s[MAXSTRING] = "this is s";
+    my_strcat(s, " and this is t\n");
+    printf(s);
+    return 0;
+}
+
+void my_strcat(char *s, char *t) {
+    while(*s) {
+        s++;
+    }
+    while(*s++ = *t++)
+        ;
+}
